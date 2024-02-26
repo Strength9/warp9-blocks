245b49dff7e11894c0e55c9d208544646d0659e6
Strength9 <Strength9@users.noreply.github.com>
2024-02-26 21:53:44 +0000

    css work to remove crap from the front

--- functions.php
+++ functions.php
@@ -45,4 +45,83 @@ require_once get_template_directory() . '/blocks/register-blocks.php';
 // require_once get_template_directory() . '/inc/theme-custom-functions.php';
 
 
+
+
+
+function custom_render_block_core_group (
+	string $block_content, 
+	array $block
+): string 
+{
+	if (
+		$block['blockName'] === 'core/group' && 
+		!is_admin() &&
+		!wp_is_json_request()
+	) {
+		$html = '';
+
+		$block['attrs']['className'] ??= '';
+
+		// Add background color to the page section
+		//$block['attrs']['className'] .= ' theme--' . ($block['attrs']['backgroundColor'] ?? 'white');
+
+		$html .= '<section class="group ' . $block['attrs']['className'] . '">' . "\n";
+		$html .= '<div class="container">' . "\n";
+
+		if (isset($block['innerBlocks'])) {
+			foreach ($block['innerBlocks'] as $inner_block) {
+				$html .= render_block($inner_block);
+			}
+		}
+
+		$html .= '</div><!--/ .container -->' . "\n";
+		$html .= '</section><!--/ .group -->' . "\n";
+
+		return $html;
+	}
+
+	return $block_content;
+}
+
+add_filter('render_block', 'custom_render_block_core_group', null, 2);
+
+
+
+remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
+remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
+
+add_action( 'wp_enqueue_scripts', function() {
+  wp_dequeue_style( 'wp-block-library' );
+  wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'classic-theme-styles' );
+} );
+
+add_filter( 'should_load_separate_core_block_assets', '__return_false' );
+
+
+
+
+
+
+
+
+
+function prefix_remove_core_block_styles() {
+	global $wp_styles;
+
+	foreach ( $wp_styles->queue as $key => $handle ) {
+		if ( strpos( $handle, 'wp-block-' ) === 0 ) {
+			wp_dequeue_style( $handle );
+		}
+	}
+}
+add_action( 'wp_enqueue_scripts', 'prefix_remove_core_block_styles',100 );
+
+// This line is preferably be added to your theme's functions.php file
+// with other add_theme_support() function calls.
+add_theme_support( 'disable-layout-styles' );
+// These two lines will probably not be necessary eventually
+remove_filter( 'render_block', 'wp_render_layout_support_flag', 10, 2 );
+remove_filter( 'render_block', 'gutenberg_render_layout_support_flag', 10, 2 );
 ?>
--- inc/theme-enques.php
+++ inc/theme-enques.php
@@ -9,7 +9,6 @@ remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
 
 function warp9_scripts()  { 
 	//Javascript
-	wp_enqueue_script( 'ws9-thp-splide', get_template_directory_uri() . '/assets/js/thirdparty/splide.js','','',true); 
 	wp_enqueue_script( 'ws9-ajax', get_template_directory_uri() . '/assets/js/script.js','','',true); 
 	// New Enques Here --
 
@@ -23,8 +22,6 @@ function warp9_scripts()  {
 
 	wp_enqueue_style('warp9-style', get_stylesheet_directory_uri() . '/style.css?v='.rand(111,999));
 	wp_enqueue_script( 'jquery-core' );
-	wp_dequeue_style( 'classic-theme-styles' );
-	wp_dequeue_style( 'wp-block-library' );
 	wp_dequeue_style('hoverIntent');
 }
 add_action( 'wp_enqueue_scripts', 'warp9_scripts' ); 
@@ -34,9 +31,9 @@ add_action( 'wp_enqueue_scripts', 'warp9_scripts' );
 /* Css Control for front end of site.
 /*-----------------------------------------------------------------------------------*/
 
-	add_filter( 'should_load_separate_core_block_assets', '__return_true' );
-	add_filter( 'styles_inline_size_limit', function() {
-		return 50000; // Size in bytes.
-	});
+	// add_filter( 'should_load_separate_core_block_assets', '__return_true' );
+	// add_filter( 'styles_inline_size_limit', function() {
+	// 	return 50000; // Size in bytes.
+	// });
 
 ?>
